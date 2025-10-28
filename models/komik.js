6f2d9de953e31359d9e0c457cccd106074420dfc
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-28 10:02:30 +0700

    mendefinisikan model sequelize(struktur tabel di)ORM

---
+++ models/komik.js
@@ -0,0 +1,20 @@
+module.export=(sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id:{
+            type : DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey: true
+        },
+        title:{ 
+            type: DataTypes.STRING,
+        },
+        description:{
+            type:DataTypes.STRING,
+        },
+        author:{
+            type:DataTypes.STRING,
+        }
+    });
+    return Komik;
+
+}
